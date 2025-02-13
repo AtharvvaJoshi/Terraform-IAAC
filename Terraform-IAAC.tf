96048ac3281ed3b5a12a134b0c4aaf03ed5322f3
Atharva Joshi <joshi.atharva123@gmail.com>
2025-02-13 12:04:37 +0530

    Added private subnets accross 3 AZ

--- Terraform-IAAC.tf
+++ Terraform-IAAC.tf
@@ -32,3 +32,24 @@ resource "aws_subnet" "public_subnet_3" {
   availability_zone = "us-east-1c"
   tags = { Name = "PublicSubnet3" }
 }
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "us-east-1a"
+  tags = { Name = "PrivateSubnet1" }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.5.0/24"
+  availability_zone = "us-east-1b"
+  tags = { Name = "PrivateSubnet2" }
+}
+
+resource "aws_subnet" "private_subnet_3" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.6.0/24"
+  availability_zone = "us-east-1c"
+  tags = { Name = "PrivateSubnet3" }
+}
