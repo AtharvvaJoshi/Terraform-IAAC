8bfe22bdc3ae1919042ac1911a5897d2a6ab578c
Atharva Joshi <joshi.atharva123@gmail.com>
2025-02-13 17:17:19 +0530

    created a SG for web access

--- Terraform-IAAC.tf
+++ Terraform-IAAC.tf
@@ -79,3 +79,10 @@ resource "aws_route_table_association" "public_assoc_3" {
   subnet_id = aws_subnet.public_subnet_3.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.main.id
+  ingress { from_port = 80 to_port = 80 protocol = "tcp" cidr_blocks = ["0.0.0.0/0"] }
+  egress { from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"] }
+  tags = { Name = "WebSecurityGroup" }
+}
