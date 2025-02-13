6ae50b89c141a28566ced403cd03b667e7513e4e
Atharva Joshi <joshi.atharva123@gmail.com>
2025-02-13 17:08:02 +0530

    Attached public subnet to the route table

--- Terraform-IAAC.tf
+++ Terraform-IAAC.tf
@@ -64,3 +64,18 @@ resource "aws_route_table" "public_rt" {
   route { cidr_block = "0.0.0.0/0" gateway_id = aws_internet_gateway.gw.id }
   tags = { Name = "PublicRouteTable" }
 }
+
+resource "aws_route_table_association" "public_assoc_1" {
+  subnet_id = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_assoc_2" {
+  subnet_id = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_assoc_3" {
+  subnet_id = aws_subnet.public_subnet_3.id
+  route_table_id = aws_route_table.public_rt.id
+}
