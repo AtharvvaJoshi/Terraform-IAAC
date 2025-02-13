9dd0bf1c7df3494dd7897564be93f1399ac30e96
Atharva Joshi <joshi.atharva123@gmail.com>
2025-02-13 16:47:25 +0530

    Added Public route table

--- Terraform-IAAC.tf
+++ Terraform-IAAC.tf
@@ -58,3 +58,9 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
   tags = { Name = "InternetGateway" }
 }
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+  route { cidr_block = "0.0.0.0/0" gateway_id = aws_internet_gateway.gw.id }
+  tags = { Name = "PublicRouteTable" }
+}
