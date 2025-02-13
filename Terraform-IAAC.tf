4e23ab6c71fb4d193e0de330402fc88e642a98cf
Atharva Joshi <joshi.atharva123@gmail.com>
2025-02-13 11:46:34 +0530

    Added public subnets accross 3 AZ

--- Terraform-IAAC.tf
+++ Terraform-IAAC.tf
@@ -8,3 +8,27 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
   tags = { Name = "MainVPC" }
 }
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+  tags = { Name = "PublicSubnet1" }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1b"
+  tags = { Name = "PublicSubnet2" }
+}
+
+resource "aws_subnet" "public_subnet_3" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1c"
+  tags = { Name = "PublicSubnet3" }
+}
